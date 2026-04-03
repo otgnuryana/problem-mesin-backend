94656807c41ea1c4b2cc4e2d8def85720e6f082b
Nuryana <43329057+otgnuryana@users.noreply.github.com>
2026-04-03 15:00:18 +0700

    Fix debounce arduino.ino

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -4,6 +4,11 @@ const int pinRepair = 3;
 bool sudahStart = false;
 bool sudahRepair = false;
 
+unsigned long lastChange = 0;
+const int debounceDelay = 80;
+
+bool lastStart = HIGH;
+bool lastRepair = HIGH;
 
 void setup() {
   pinMode(pinStart, INPUT_PULLUP);
@@ -12,21 +17,37 @@ void setup() {
 }
 
 void loop() {
-  bool startAktif = digitalRead(pinStart) == LOW;
-  bool repairAktif = digitalRead(pinRepair) == LOW;
+  bool startNow = digitalRead(pinStart);
+  bool repairNow = digitalRead(pinRepair);
 
-  if (startAktif && repairAktif && !sudahStart) {
-    Serial.println("START");
-    sudahStart = true;
-    sudahRepair = false;
-  } else if (!startAktif && repairAktif && !sudahRepair) {
-    Serial.println("REPAIR-START");
-    sudahRepair = true;
-  } else if (!startAktif && !repairAktif && (sudahStart || sudahRepair)) {
-    Serial.println("FINISH");
-    sudahStart = false;
-    sudahRepair = false;
+  // debounce detect perubahan
+  if (startNow != lastStart || repairNow != lastRepair) {
+    lastChange = millis();
   }
 
-  delay(300);
+  if ((millis() - lastChange) > debounceDelay) {
+
+    bool startAktif = startNow == LOW;
+    bool repairAktif = repairNow == LOW;
+
+    if (startAktif && repairAktif && !sudahStart) {
+      Serial.println("START");
+      sudahStart = true;
+      sudahRepair = false;
+
+    } else if (!startAktif && repairAktif && !sudahRepair && sudahStart) {
+      Serial.println("REPAIR-START");
+      sudahRepair = true;
+
+    } else if (!startAktif && !repairAktif && (sudahStart || sudahRepair)) {
+      Serial.println("FINISH");
+      sudahStart = false;
+      sudahRepair = false;
+    }
+  }
+
+  lastStart = startNow;
+  lastRepair = repairNow;
+
+  delay(10); // dari 300 → 10 (WAJIB)
 }
